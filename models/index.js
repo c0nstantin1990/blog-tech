03323012bcb1f6c029f03b2593b95fa5a5009586
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-21 11:11:53 -0400

    added index.js for all models

---
+++ models/index.js
@@ -0,0 +1,43 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// Define associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  hooks: true,
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  hooks: true,
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  hooks: true,
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+  hooks: true,
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  hooks: true,
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+  hooks: true,
+});
+
+// Export the modules
+module.exports = { User, Post, Comment };
