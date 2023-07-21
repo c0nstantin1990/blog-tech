b9e238a0854c4692dde3c35ed5e1769a65487288
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-21 09:44:19 -0400

    added addPost.js

---
+++ public/js/addPost.js
@@ -0,0 +1,28 @@
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector(
+      'textarea[name="post-text"]'
+    ).value;
+
+    try {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({ title, post_text }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        document.location.replace("/dashboard");
+      } else {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+    } catch (error) {
+      alert(`Error: ${error.message}`);
+    }
+  });
